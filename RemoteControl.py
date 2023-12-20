ac9f15a61bcb16a536ab5e3f8daa64e85d23658e
Chiakai <37283858+Chiakai-Chang@users.noreply.github.com>
2023-12-20 23:36:25 +0800

    (Add&Fix)基本上滑鼠移動與開關做好，再來是移動量的優化

--- RemoteControl.py
+++ RemoteControl.py
@@ -16,17 +16,17 @@ class servo_control:
     name = ''
     
     # 版本號
-    version = '20231220_0'
+    version = '20231220_1'
     
     # Pin 
-    Pin_up_down = 1      # 0 ~ 180
-    Pin_left_right = 2   # 0 ~ 180
-    Pin_on_off = 3       # 70 == on, 180 == off
+    Pin_up_down = 1      # 橘(銀)線, PIN5, 0 ~ 180
+    Pin_left_right = 2   # 綠線, PIN11, 0 ~ 180
+    Pin_on_off = 3       # 橘線, PIN3, 70 == on, 180 == off
 
     # 紀錄 servo 目前數值
     current_ud_value = 90
     current_lr_value = 90
-    current_onoff_value = 180
+    current_onoff = 1
     
     # Tracking Box 左上、右下座標
     frame = None
@@ -57,7 +57,9 @@ class servo_control:
     roi_bbox_left_up = [] 
     roi_bbox_right_down = []
     
+    # 滑鼠事件
     mouse_event = None 
+    mouse_clicked_not_up = False
     
     def __init__(
             self, 
@@ -74,8 +76,14 @@ class servo_control:
         self.Baud_rate = Baud_rate
         self.camera_id = camera_id
         
+        # Serial 溝通物件
+        self.controller = serial.Serial(self.COM_PORT, self.Baud_rate, timeout=1)
+        
+        print('>>Servo 已啟動...')
+        self.reset_servo()
+        
         # CV2 的鏡頭物件
-        print('>>啟動中，請稍候...')
+        print('>>鏡頭啟動中，請稍候...')
         self.cap = cv2.VideoCapture(self.camera_id)
         if not self.cap.isOpened():
             print(">>Cannot open camera")
@@ -92,17 +100,11 @@ class servo_control:
         self.center_lr =  self.video_width / 2
         
         # 取得影像尺寸與 0 ~ 180 之間的 ratio
-        self.ud_ratio = 180 / self.video_height
-        self.lr_ratio = 180 / self.video_width
+        self.ud_ratio = 180 / (self.video_height * 9) 
+        self.lr_ratio = 180 / (self.video_width * 9)
         
         # 移動的倍率(好像用不到)
         self.move_rate = move_rate
-        
-        # Serial 溝通物件
-        self.controller = serial.Serial(self.COM_PORT, self.Baud_rate, timeout=1)
-        
-        print('>>Servo 已啟動...')
-        self.reset_servo()
         
         # 創建追蹤器
         # 特別註記
@@ -111,64 +113,72 @@ class servo_control:
         self.tracker = cv2.TrackerCSRT_create()  
         
         self.run()
-        print('>>已開啟鏡頭視窗...')        
+        print('>>已開啟鏡頭...')        
         
     def reset_servo(self):
         # 將攝影機 servo 置中
         self.current_ud_value = 90
         self.current_lr_value = 90
-        self.current_onoff_value = 180
+        self.current_onoff = 0
         
         for p, v in [
                 (self.Pin_up_down, self.current_ud_value),
                 (self.Pin_left_right, self.current_lr_value),
-                (self.Pin_on_off, self.current_onoff_value),
+                (self.Pin_on_off, 180),
                 ]:
             command = f"{p} {v}\n"
-            print(f'command = {command}')
+            print(f'command = {command}'.strip())
             self.controller.write(command.encode())
         print('>>Servo 位置已歸零...')
     
     def light_on(self):
-        self.servo_onoff_value = 70
-        command = f"{self.Pin_on_off} {self.servo_onoff_value}\n"
-        print(f'command = {command}')
+        self.current_onoff = 1
+        command = f"{self.Pin_on_off} 70\n"
+        print(f'command = {command}'.strip())
         self.controller.write(command.encode())
     
     def light_off(self):
-        self.current_onoff_value = 180
-        command = f"{self.Pin_on_off} {self.current_onoff_value}\n"
-        print(f'command = {command}')
+        self.current_onoff = 0
+        command = f"{self.Pin_on_off} 180\n"
+        print(f'command = {command}'.strip())
         self.controller.write(command.encode())
     
-    def move_updown(self, value):
-        move_value = value * self.ud_ratio
-        if abs(move_value) > self.allow_diff:
-            new_ud_value = self.current_ud_value + move_value
-            if 0 <= new_ud_value <= 180:
-                self.current_ud_value = new_ud_value
-                command = f"{self.Pin_up_down} {self.current_ud_value}\n"
-                print(f'command = {command}')
-                self.controller.write(command.encode())
-            else:
-                print(f'[Limit] Servo ud 想移動到: {new_ud_value}, 已超出可移動範圍...')
+    def count_diff(
+            self,
+            x = None,
+            y = None,
+            ):
+        if x and y:
+            self.diff_lr =  x - self.center_lr
+            self.diff_ud =  self.center_ud - y      
         else:
-            print(f'[Limit] Servo ud 想微微移動: {move_value} 的量, 移動幅度太小，略...')
-                
-    def move_leftright(self, value):
-        move_value = value * self.lr_ratio
-        if abs(move_value) > self.allow_diff:
-            new_lr_value = self.current_lr_value + move_value
-            if 0 <= new_lr_value <= 180:
-                self.current_lr_value = new_lr_value
-                command = f"{self.Pin_left_right} {self.current_lr_value}\n"
-                print(f'command = {command}')
-                self.controller.write(command.encode())
-            else:
-                print(f'[Limit] Servo lr 想移動到: {new_lr_value}, 已超出可移動範圍...')
-        else:
-            print(f'[Limit] Servo lr 想微微移動: {move_value} 的量, 移動幅度太小，略...')
+            self.diff_ud =  self.tracking_ud - self.center_ud
+            self.diff_lr =  self.center_lr - self.tracking_lr   
+        print(f'center_ud = {self.center_ud}')
+        print(f'center_lr = {self.center_lr}')
+        print(f'diff_ud = {self.diff_ud}')
+        print(f'diff_lr = {self.diff_lr}')
     
+    def move_updown(self, value):
+        new_ud_value = self.current_ud_value + (value * self.move_rate)
+        if 0 <= new_ud_value <= 180:
+            self.current_ud_value = new_ud_value
+            command = f"{self.Pin_up_down} {self.current_ud_value}\n"
+            print(f'command = {command}'.strip())
+            self.controller.write(command.encode())
+        else:
+            print(f'[Limit] Servo ud 想移動到: {new_ud_value}, 已超出可移動範圍...')
+
+    def move_leftright(self, value):
+        new_lr_value = self.current_lr_value + (value * self.move_rate)
+        if 0 <= new_lr_value <= 180:
+            self.current_lr_value = new_lr_value
+            command = f"{self.Pin_left_right} {self.current_lr_value}\n"
+            print(f'command = {command}'.strip())
+            self.controller.write(command.encode())
+        else:
+            print(f'[Limit] Servo lr 想移動到: {new_lr_value}, 已超出可移動範圍...')
+
     def mouse_action(self, event, x, y, flags, frame):
         self.frame = frame
         self.mouse_xy = [x, y]
@@ -182,6 +192,9 @@ class servo_control:
            # 計算可能要畫 ROI BBox
            self.roi_wait_start = time.time()
            self.roi_click_down = [x, y]
+           self.count_diff(x, y)
+           
+           self.mouse_clicked_not_up = True
            
            # 如果開啟著 Tracking 點 Box 就關閉 Tracking
            if self.tracking:
@@ -191,7 +204,20 @@ class servo_control:
                        self.tracking = False
                        self.light_off()
         
+        if event == cv2.EVENT_RBUTTONDOWN:
+            # 右鍵開關燈
+            print(f'>>Event: {event}')
+            print(f'Position X: {x}')
+            print(f'Position Y: {y}')
+            
+            if self.current_onoff:
+                self.light_off()
+            else:
+                self.light_on()
+        
         if event == cv2.EVENT_LBUTTONUP:
+            self.mouse_clicked_not_up = False
+            
             # 計算是否是在畫 ROI BBox
             self.roi_wait_count = time.time() - self.roi_wait_start
             if self.roi_wait_count >= self.roi_wait_limit:
@@ -207,17 +233,13 @@ class servo_control:
                 self.roi_bbox_right_down = [right, down]
                 print(f'self.roi_bbox_left_up = {self.roi_bbox_left_up}')
                 print(f'self.roi_bbox_right_down = {self.roi_bbox_right_down}')
-                area = cv2.selectROI(self.name, frame, showCrosshair=False, fromCenter=False)
-                print(area)
-                input()
-                self.tracker.init(frame, area)    # 初始化追蹤器
-                self.tracking = True              # 設定可以開始追蹤
-                self.light_on()
-    
-    def count_diff(self):
-        self.diff_ud =  self.tracking_ud - self.center_ud
-        self.diff_lr =  self.center_lr - self.tracking_lr      
-    
+                #area = cv2.selectROI(self.name, frame, showCrosshair=False, fromCenter=False)
+                #print(area)
+                #input()
+                #self.tracker.init(frame, area)    # 初始化追蹤器
+                #self.tracking = True              # 設定可以開始追蹤
+                #self.light_on()
+            
     def run(self):
         try:
             while True:
@@ -234,13 +256,17 @@ class servo_control:
                     pass
                 #frame = cv2.resize(frame,(540,300))  # 縮小尺寸，加快速度
                 # 將按鈕說明寫在圖上
-                cv2.putText(frame, 'Press "q" to quit...', (10, int(self.video_height-20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (210, 210, 210), 2)
+                cv2.putText(frame, 'Buttons:', (10, int(self.video_height-80)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (210, 210, 210), 2)
+                cv2.putText(frame, '"r": Reset Camera', (10, int(self.video_height-50)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (210, 210, 210), 2)
+                cv2.putText(frame, '"q": quit...', (10, int(self.video_height-20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (210, 210, 210), 2)
 
                 cv2.imshow('Remote Guard', frame)
                 keyName = cv2.waitKey(1)  & 0xFF
                 
                 if keyName == ord('q'):
                     break
+                if keyName == ord('r'):
+                    self.reset_servo()
                 if keyName == ord('a'):
                     area = cv2.selectROI('Remote Guard', frame, showCrosshair=False, fromCenter=False)
                     print(f'area = {area}')
@@ -285,7 +311,22 @@ class servo_control:
                         # 移動鏡頭
                         self.move_updown(self.diff_ud)
                         self.move_leftright(self.diff_lr)
+                   
+                if self.mouse_clicked_not_up:
+                    # 點擊不放，就持續往該點位方向移動
+                    self.count_diff(self.mouse_xy[0], self.mouse_xy[1])
                     
+                    # 移動鏡頭
+                    if self.diff_ud < 0:
+                        self.move_updown(1)
+                    elif self.diff_ud > 0:
+                        self.move_updown(-1)
+                    
+                    if self.diff_lr < 0:
+                        self.move_leftright(1)
+                    elif self.diff_lr > 0:
+                        self.move_leftright(-1)
+                        
             self.cap.release()
             cv2.destroyAllWindows()
         except:
